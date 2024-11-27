8521a2b605eb674e4d15dcf2d48db252dd52a724
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-27 16:17:22 +0100

    Added role-based access control: Admin can manage events, users can view events

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
-
 Route::get('/', function () {
     return view('home');
 });
@@ -14,4 +13,16 @@ Auth::routes();
 Route::get('/home', function () {
     return redirect()->route('events.index');
 })->name('home');
-Route::resource('events', EventController::class);
+
+// Rotte protette per gli amministratori
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+    Route::post('/events', [EventController::class, 'store'])->name('events.store');
+    Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+    Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
+    Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+});
+
+// Rotte accessibili a tutti gli utenti autenticati o ospiti
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
