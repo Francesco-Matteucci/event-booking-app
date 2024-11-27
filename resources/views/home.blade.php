8e0a0cc1bde4899297e4392d0e83283dceeb03bc
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-27 15:49:53 +0100

    Updated web.php to set home as default view and customized home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1>Benvenuto nel Sistema di Gestione Eventi!</h1>
+    <p>Gestisci i tuoi eventi e le prenotazioni in modo semplice e veloce.</p>
+    <a href="{{ route('events.index') }}" class="btn btn-primary">Visualizza Eventi</a>
 </div>
 @endsection
