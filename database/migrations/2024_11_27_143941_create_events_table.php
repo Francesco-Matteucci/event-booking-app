d89c0c40d79a9cea2698dbb6dcb6c7e5eb2f3161
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-27 15:42:45 +0100

    Added Event model and migration with table creation

---
+++ database/migrations/2024_11_27_143941_create_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nome dell'evento
+            $table->text('description'); // Descrizione dell'evento
+            $table->date('date'); // Data dell'evento
+            $table->integer('available_seats'); // Posti disponibili
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
